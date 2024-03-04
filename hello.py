2c6606979c8bb67b2e480e424ab7d848f794fdc7
Damian Jaramillo <damianjaramillo@gmail.com>
2024-03-04 18:09:32 -0500

    mi trabajo de heloo world

--- hello.py
+++ hello.py
@@ -1,2 +1,6 @@
-def hello_world():
-    return "Hello!"
+def welcome_message():
+    name = input("Ingrese su nombre: ")
+    print("Bienvenido man : ", name, "al curso de Django y React!")
+														
+if __name__ == "__main__":
+    welcome_message()
